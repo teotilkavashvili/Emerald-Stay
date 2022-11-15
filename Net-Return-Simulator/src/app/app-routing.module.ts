f07fcb6e2e51d00e6c590a62b7b38978ed2e5a0d
teotilkavashvili <teonatilkavashvili@gmail.com>
2022-11-15 15:49:23 +0400

    created navigation

--- Net-Return-Simulator/src/app/app-routing.module.ts
+++ Net-Return-Simulator/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ROUTES } from './routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: ROUTES.HOME,
+    pathMatch: 'full',
+  },
+  // {
+  //   path: ROUTES.CANDIDATES,
+  //   loadChildren: () =>
+  //     import('./pages/candidates/candidate.module').then((m) => m.CandidateModule),
+  // },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
